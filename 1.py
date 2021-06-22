58f0198a7bcdf675a038ce2d45c2d93817e70c82
mirittw <mirittw@gmail.com>
2021-06-22 16:41:03 +0300

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,17 @@
+print("hello hello")
+a = 4
+print(a)
+b = "miritush"
+print(b)
+c = ["aa", 4, True]
+print(c)
+c[2] = "bla"
+print(c[2])
+d = ("bb", 4, True)
+print(d)
+# d[2] = "google"
+print(d)
+person1 = ["mirit", "twig", 31, False]
+person2 = {"First Name": "mirit", "Last Name": "twig", "Age": 31, "Is Male": False}
+print(person2["Age"])
+print(person2.keys())
